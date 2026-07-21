6f7806aa9d24a940225bbe7dfb0e8ff38c0fe988
Wiktor1990 <wicktor1kent@gmail.com>
2026-07-21 18:13:45 +0200

    fixed_cipher

--- hw_16.js
+++ hw_16.js
@@ -72,7 +72,7 @@ let codeWord3 = "огурцы";
 let codeWord4 = "липкие";
 let codeWord5 = "?!";
 
-let cipher =
+const cipher =
   codeWord1[1] + codeWord2[1] + codeWord3[1] + codeWord4[1] + codeWord5[1];
 console.log(cipher);
 
