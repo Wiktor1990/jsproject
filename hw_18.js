ab330cbf03ab4a0490b3a0ee7834690a362fac09
Wiktor1990 <wicktor1kent@gmail.com>
2026-07-28 22:12:38 +0200

    add_hw_18

---
+++ hw_18.js
@@ -0,0 +1,77 @@
+function getSum(n) {
+  let sum = 0;
+  for (let i = 0; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+
+console.log(getSum(100));
+
+function calculateOverpayment(loanAmount) {
+  const interestRate = 0.17;
+  const years = 5;
+
+  return loanAmount * interestRate * years;
+}
+
+console.log(calculateOverpayment(500000));
+
+function trimString(str, fromIndex, toIndex) {
+  return str.substring(fromIndex, toIndex);
+}
+
+const text = "Привет, мир!";
+console.log(trimString(text, 0, 6));
+console.log(trimString(text, 8, 11));
+
+function getSumNumbers(number) {
+  const str = String(number);
+  let sum = 0;
+
+  for (let i = 0; i < str.length; i++) {
+    sum += Number(str[i]);
+  }
+
+  return sum;
+}
+
+console.log(getSumNumbers(2021));
+
+function getSum1(a, b) {
+  const start = Math.min(a, b);
+  const end = Math.max(a, b);
+
+  let sum = 0;
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
+}
+
+console.log(getSum1(1, 0));
+console.log(getSum1(1, 2));
+console.log(getSum1(1, 1));
+console.log(getSum1(-1, 0));
+console.log(getSum1(-1, 2));
+
+function foo() {
+  console.log("foo");
+}
+
+function boo() {
+  console.log("boo");
+}
+
+function fooBoo(flag, fooFunc, booFunc) {
+  if (flag) {
+    fooFunc();
+  } else {
+    booFunc();
+  }
+}
+
+fooBoo(true, foo, boo);
+fooBoo(false, foo, boo);
