6a8b81db8b07c3a48b8aec4777e2bf4d440786e2
Wiktor1990 <wicktor1kent@gmail.com>
2026-08-01 20:17:35 +0200

    add_hw_19

---
+++ hw_19.js
@@ -0,0 +1,86 @@
+const user = {
+  name: "John",
+  age: 30,
+};
+
+delete user.age;
+delete user.name;
+console.log(user);
+
+const user2 = {
+  name: "Smith",
+  age: 25,
+};
+
+if ("age" in user2) {
+  console.log(true);
+}
+
+const student = {
+  name: "John",
+  age: 19,
+  isHappy: true,
+};
+
+for (let key in student) {
+  console.log(`Ключ: ${key}, значение: ${student[key]}`);
+}
+
+const colors = {
+  "ru pum pu ru rum": {
+    red: "красный",
+    green: "зеленый",
+    blue: "синий",
+  },
+};
+
+console.log(colors["ru pum pu ru rum"].red);
+console.log(colors["ru pum pu ru rum"].blue);
+
+let salaries = {
+  andrey: 500,
+  sveta: 413,
+  anton: 987,
+  igor: 664,
+  alexandra: 19,
+};
+
+const salaryValues = Object.values(salaries);
+const totalSalary = salaryValues.reduce((sum, current) => sum + current, 0);
+let averageSalary = totalSalary / salaryValues.length;
+console.log(`Средняя зарплата: ${averageSalary}`);
+
+const registerLogin = prompt("Регистрация: Введите логин");
+const registerPassword = prompt("Регистрация: Введите пароль");
+
+const userCredentials = {
+  login: registerLogin,
+  password: registerPassword,
+};
+
+alert("Регистрация прошла успешно!");
+
+const loginInput = prompt("Вход: Введите логин");
+const passwordInput = prompt("Вход: Введите пароль");
+
+if (
+  loginInput === userCredentials.login &&
+  passwordInput === userCredentials.password
+) {
+  alert("Добро пожаловать");
+} else {
+  alert("Неверный логин или пароль!");
+}
+
+let student1 = {
+  name: "Polina",
+  age: 27,
+};
+let student2 = {
+  name: "Polina",
+  age: 27,
+};
+
+const isEqual =
+  student1.name === student2.name && student1.age === student2.age;
+console.log(isEqual);
