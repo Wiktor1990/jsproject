2b7d33abbf77504240a8c3dcc23b9db93e077629
Wiktor1990 <wicktor1kent@gmail.com>
2026-08-19 22:32:36 +0200

    add_todo_part2

--- todo_list/script.js
+++ todo_list/script.js
@@ -1,3 +1,11 @@
+const createCustomElement = (tagName, className, textContent = "") => {
+  const element = document.createElement(tagName);
+  if (className) element.className = className;
+
+  if (textContent) element.textContent = textContent;
+  return element;
+};
+
 function initTodoApp() {
   const root = document.getElementById("root");
   if (!root) return;
@@ -18,7 +26,65 @@ function initTodoApp() {
   addBtn.className = "btn-accent";
   addBtn.textContent = "Add";
 
-  container.append(deleteAllBtn, input, addBtn);
+  const inputGroup = createCustomElement("div", "input-group");
+  inputGroup.append(deleteAllBtn, input, addBtn);
+  container.append(inputGroup);
+
+  const todoList = createCustomElement("ul", "todo-list");
+  container.append(todoList);
   root.append(container);
+
+  const handleAddTask = () => {
+    const text = input.value.trim();
+    if (text !== "") {
+      createTodoItem(text, todoList);
+      input.value = "";
+      input.focus();
+    }
+  };
+
+  addBtn.addEventListener("click", handleAddTask);
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      handleAddTask();
+    }
+  });
+
+  deleteAllBtn.addEventListener("click", () => {
+    todoList.innerHTML = "";
+  });
 }
 initTodoApp();
+
+const createTodoItem = (text, todoList) => {
+  const checkBtn = createCustomElement("button", "check-btn", "✓");
+  const textDiv = createCustomElement("div", "todo-text", text);
+  const deleteBtn = createCustomElement("button", "delete-btn", "X");
+
+  const dateString = new Date().toLocaleDateString("ru");
+  const dateDiv = createCustomElement("div", "todo-date", dateString);
+
+  const li = createCustomElement("li", "todo-item");
+
+  li.append(checkBtn, textDiv, deleteBtn, dateDiv);
+  todoList.append(li);
+
+  deleteBtn.addEventListener("click", () => {
+    li.remove();
+  });
+
+  checkBtn.addEventListener("click", () => {
+    const isDone = textDiv.style.textDecoration === "line-through";
+
+    if (isDone) {
+      textDiv.style.textDecoration = "none";
+      textDiv.style.opacity = "1";
+      li.classList.remove("completed");
+    } else {
+      textDiv.style.textDecoration = "line-through";
+      textDiv.style.opacity = "0.5";
+      li.classList.add("completed");
+    }
+  });
+};
